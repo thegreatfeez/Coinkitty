cab2966f3f30533a910f212d0e4658602f6b6071
thegreatfeez <adamsafeez4@gmail.com>
2025-09-15 13:18:13 +0100

    feat: integrate CoinGecko API to display live crypto data on homepage

---
+++ api.js
@@ -0,0 +1,8 @@
+
+export async function fetchCryptoPrices() {
+const response = await fetch(
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1'
+);
+const data = await response.json();
+return data;
+}
