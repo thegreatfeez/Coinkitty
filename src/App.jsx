c143c88eed5fc1d4004d3ff49c3ae0dcdd5f9793
thegreatfeez <adamsafeez4@gmail.com>
2025-09-23 19:09:42 +0100

    feat: portfolio feeds

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import Home from "./pages/Home";
 import Insight from "./pages/Insight";
 import Learn from "./pages/Learn";
 import TokenDetails from "./pages/TokenDetails";
+import { PortfolioProvider } from "./contexts/PortfolioContext";
+import Portfolio from "./pages/Portfolio"
 
 function App() {
   return (
+  <PortfolioProvider>
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<div>Portfolio Page</div>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
         <Route path="/insights" element={<Insight />} />
         <Route path="/learn" element={<Learn />} />
         <Route path="/token/:tokenId" element={<TokenDetails />} />
         <Route path="/Login" element={<div>Please Login</div>} />
       </Routes>
     </Router>
+  </PortfolioProvider>
   );
 }
 
