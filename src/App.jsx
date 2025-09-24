f1b2e944a1c68435b7c8fb2a9adc0cdbc226437f
thegreatfeez <adamsafeez4@gmail.com>
2025-09-24 15:25:09 +0100

    setp: auth && login

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Learn from "./pages/Learn";
 import TokenDetails from "./pages/TokenDetails";
 import { PortfolioProvider } from "./contexts/PortfolioContext";
 import Portfolio from "./pages/Portfolio"
+import Login from "./pages/Login";
+import AuthRequired from "./components/AuthRequired";
 
 function App() {
   return (
@@ -15,11 +17,16 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio/>} />
-        <Route path="/insights" element={<Insight />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/token/:tokenId" element={<TokenDetails />} />
-        <Route path="/Login" element={<div>Please Login</div>} />
+
+        <Route element={<AuthRequired/>}>
+           <Route path="portfolio" element={<Portfolio/>} />
+        </Route>
+       
+
+        <Route path="insights" element={<Insight />} />
+        <Route path="learn" element={<Learn />} />
+        <Route path="token/:tokenId" element={<TokenDetails />} />
+        <Route path="Login" element={<Login/>} />
       </Routes>
     </Router>
   </PortfolioProvider>
